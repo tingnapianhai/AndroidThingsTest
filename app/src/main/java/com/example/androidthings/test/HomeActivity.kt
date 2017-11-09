11385bc6d9e93080f8672b18f87eff65e0831f7d
Kai <tingnapianhai@gmail.com>
2017-11-09 23:48:40 +0100

    Register a button input

--- app/src/main/java/com/example/androidthings/test/HomeActivity.kt
+++ app/src/main/java/com/example/androidthings/test/HomeActivity.kt
@@ -2,6 +2,12 @@ package com.example.androidthings.test
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.GpioCallback
+import com.google.android.things.pio.PeripheralManagerService
+import java.io.IOException
+
 
 /**
  * Skeleton of an Android Things activity.
@@ -25,7 +31,48 @@ import android.os.Bundle
  */
 class HomeActivity : Activity() {
 
+  private val TAG = "HomeActivity"
+
+  private val BUTTON_PIN_NAME = "GPIO_174"
+
+  // GPIO connection to button input
+  private var mButtonGpio: Gpio? = null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    val service = PeripheralManagerService()
+    Log.d(TAG, "Available GPIO: " + service.gpioList)
+
+    try {
+      // Create GPIO connection.
+      mButtonGpio = service.openGpio(BUTTON_PIN_NAME)
+
+      // Configure as an input, trigger events on every change.
+      mButtonGpio?.setDirection(Gpio.DIRECTION_IN)
+      mButtonGpio?.setEdgeTriggerType(Gpio.EDGE_BOTH)
+      // Value is true when the pin is LOW
+      mButtonGpio?.setActiveType(Gpio.ACTIVE_LOW)
+
+      // Register the event callback.
+      mButtonGpio?.registerGpioCallback(mCallback);
+
+    } catch (e: IOException) {
+      Log.w(TAG, "Error opening GPIO", e)
+    }
+
   }
+
+  private val mCallback = object : GpioCallback() {
+    override fun onGpioEdge(gpio: Gpio?): Boolean {
+      try {
+        Log.i(TAG, "GPIO changed, button " + gpio!!.value)
+      } catch (e: IOException) {
+        Log.w(TAG, "Error reading GPIO")
+      }
+      // Return true to keep callback active.
+      return true
+    }
+  }
+
 }
